2c45f5a95857fbc3cd7d2ead429269a0b66a59cb
Zachary Kaye <zck10@uclive.ac.nz>
2021-04-15 12:50:17 +1200

    Wrote packet functions required for initial setup and began working on the functions required for transmission.

--- Router.py
+++ Router.py
@@ -16,6 +16,8 @@ class Router:
         self.valid_packet = False
         self.error_msg = ""
         self.routing_table = dict()
+        self.response_packet = dict()
+        self.destination_router_id = ""
 
     def validate_response_packet(self, packet):
         """
@@ -59,9 +61,10 @@ class Router:
                     self.valid_packet = True
                     self.error_msg = ""
 
-    def populate_routing_table(self):
+    def populate_routing_table(self, setup_packets):
         """
-        Sets up the initial routing table for the router, using the input and output ports.
+        Sets up the initial routing table for the router.
+        These are the direct connections to the router and will have a metric of 1.
 
         Routing Table Entry:
 
@@ -69,11 +72,66 @@ class Router:
         - metric, cost of sending datagram from router to destination.
         - next_router_id, the router id of the next router along the path to the destination,
         empty string if directly connected (in our setup).
-        - flag, indicates whether the route has changed recently.
+        - flag, indicates whether the route has changed recently. (True or False)
         """
+        for packet in setup_packets:
+            router_id = packet['router_id']
+            self.routing_table[router_id] = {'destination_router-id': router_id, 'metric': 1,
+                                             'next_router_id': "", 'flag': True}
 
-        for port in self.input_ports:
-            self.routing_table['a'] = 'a'
+    def create_setup_response_packet(self):
+        """Create a RIP response packet that will be used for setup."""
+        self.response_packet = dict()
+
+        self.response_packet['command'] = 2
+        self.response_packet['version'] = 2
+        self.response_packet['router_id'] = self.router_id
+        self.response_packet['entry1'] = {self.router_id, 0}  # Metric is zero because router is connected to its self.
+
+        return self.response_packet
+
+    def create_response_packet(self):
+        """Creates a RIP response packet based on the specifications."""
+        self.response_packet = dict()
+
+        self.response_packet['command'] = 2
+        self.response_packet['version'] = 2
+        self.response_packet['router_id'] = self.router_id
+
+        entry_number = 1
+        for entry in self.routing_table:
+            # Routes learnt from neighbor included in updates sent to that neighbor.
+            # "Split horizon with poisoned reverse" is used.
+            # TODO this will probably have to be changed, because self.destination_router_id is not updated,
+            #  maybe pass in destination_router_id.
+            if entry['next_router_id'] == self.destination_router_id:
+                # Sets their metrics to "infinity"/unreachable as required by "Split horizon with poisoned reverse"
+                entry['metric'] = 16
+            entry_access = "entry" + str(entry_number)
+            self.response_packet[entry_access] = entry
+            entry_number += 1
+
+        return self.response_packet
+
+    def read_response_packet(self, packet):
+        """Reads a RIP response packet and calls function to add/update RIP entries in routing table."""
+        self.validate_response_packet(packet)
+        distance = 1
+        # Routing update arrives from a neighbor G', add the cost associated with the network that is shared with G'.
+        # (This should be the network over which the update arrived.) Call the resulting distance D'.
+        # Compare the resulting distances with the current routing table entries. If the new distance D' for N is
+        # smaller than the existing value D, adopt the new route.That is, change the table entry for N to have metric D'
+        # and router G'.If G' is the router from which the existing route came, i.e., G' = G, then use the new metric
+        # even if it is larger than the old one.
+
+        if self.valid_packet:
+            # Update routing table
+            # if metric is smaller take that
+            return
+        else:
+            print(self.error_msg)
+            print("Discarding packet...")
+            return
 
 
 config = setup.get_config_file()
